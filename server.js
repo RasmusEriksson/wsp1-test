a18f82822d265e2cbd64786d0dd93361c7a276b7
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2025-10-07 09:38:10 +0200

    la till frukt id funktionalitet

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import nunjucks from "nunjucks"
 import morgan from "morgan"
 import fs from "fs"
+
 const app = express()
 
 app.use(morgan("dev"))
@@ -9,6 +10,8 @@ app.use(express.static("public"))
 
 import index_router from "./routes/index.js"
 import router from "./routes/index.js"
+
+const {fruits} = JSON.parse(fs.readFileSync("./data/fruit.json"))
 
 
 nunjucks.configure("views", {
@@ -20,11 +23,26 @@ nunjucks.configure("views", {
 app.use("/", index_router)
 
 router.get("/fruit", (req, res) =>{
-    const {fruits} = JSON.parse(fs.readFileSync("./data/fruit.json"))
-    
     res.render("fruit.njk", {
         fruit: fruits
     })
+})
+
+router.get("/fruit/:id", (req,res) => {
+    const selected_fruit = fruits.find(f => f.id === +req.params.id)
+
+    console.log(selected_fruit)
+
+    if (selected_fruit) {
+        console.log("hiiiiii :)")
+        res.render("single_fruit.njk", {
+            name: selected_fruit.name,
+            texture: selected_fruit.texture
+        })
+    } else {
+        console.log("baiiiiiiii :(")
+        res.status(404).json({error: "fruit not found"})
+    }
 })
 
 
